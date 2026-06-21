5ac0e7f059cc52419224443dbc127a05693a8181
Dad <dad@Deyaniras-MacBook-Air.local>
2026-06-20 20:33:36 -0700

    Add uppercase password requirement

--- password_strength_analyzer.py
+++ password_strength_analyzer.py
@@ -8,3 +8,10 @@ if len(password) >= 15:
     print("Length Requirement: PASS")
 else:
     print("Length Requirement: FAIL - Password should be at least 15 characters.")
+
+has_upper = any(char.isupper() for char in password)
+
+if has_upper:
+    print("Uppercase Requirement: PASS")
+else:
+    print("Uppercase Requirement: FAIL - Add at least one uppercase letter.")
